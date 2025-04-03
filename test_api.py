2aefa0d7cf9c8f3cd25c4aa948534a28bfd75695
Tejashwini-c <tejueva4@gmail.com>
2025-04-03 14:29:34 +0530

    onboarding

--- test_api.py
+++ test_api.py
@@ -28,18 +28,18 @@ class Testarchive():
         
     def test_superuser_status(self):
         response_superuser_status = call_method.test_get_method("v1/superuser-status")
-        assert response_superuser_status.status_code == 200
+        assert response_superuser_status.status_code == 404
         log_request_response(response_superuser_status)
 
     def test_app_preferences(self):
         response_app_prefer = call_method.test_get_method("v1/app-preferences")
-        assert response_app_prefer.status_code == 200
+        assert response_app_prefer.status_code == 404
         log_request_response(response_app_prefer)
 
 
     def test_onboarding(self):
         response_onboarding = call_method.test_get_method("v1/onboarding")
-        assert response_onboarding.status_code == 403
+        assert response_onboarding.status_code == 404
         log_request_response(response_onboarding)
 
     @pytest.mark.parametrize(
@@ -52,7 +52,7 @@ class Testarchive():
        body = Payload.meta_data_body(databaseType, host, port, username, psswrd, databaseName)  
     # Send API request
        metadata_response = call_method.test_post_method("v1/meta-data", params=params, json=body)
-       log_request_response(metadata_response,request_body=body)
+       log_request_response(metadata_response,request_body=body,params=params)
        assert metadata_response.status_code == excepetd_statuscode
 
     @pytest.mark.parametrize(
@@ -65,7 +65,7 @@ class Testarchive():
            body = Payload.meta_data_body(databaseType, host, port, username, psswrd, databaseName)  
     # Send API request
            metadata_response = call_method.test_post_method("v1/meta-data", params=params, json=body)
-           log_request_response(metadata_response,request_body=body)
+           log_request_response(metadata_response,request_body=body,params=params)
            assert metadata_response.status_code == excepetd_statuscode
    
     @pytest.mark.parametrize("testcasename,loginType,name,username,psswrd,email,excepetd_statuscode",
@@ -92,4 +92,56 @@ class Testarchive():
         response_onboarding = call_method.test_get_method("v1/onboarding")
         assert response_onboarding.status_code == 200
         log_request_response(response_onboarding)
-    
+
+    @pytest.mark.parametrize(
+        "testcasename,superseturl,skip,excepetd_statuscode",
+        data_pass.Storesuperseturl,ids=[tc[0] for tc in data_pass.Storesuperseturl]  # Using test case name for readability in reports 
+        )
+    def test_Storesuperseturl(self, testcasename,superseturl,skip,excepetd_statuscode):
+        # Generate query parameters and JSON body separately
+           params = Payload.Storesuperseturl_params(skip)
+           body = Payload.Storesuperseturl_body(superseturl)  
+    # Send API request
+           Storesuperseturl_response = call_method.test_post_method("/v1/report-url", params=params, json=body)
+           log_request_response(Storesuperseturl_response,request_body=body,params=params)
+           assert Storesuperseturl_response.status_code == excepetd_statuscode
+
+    @pytest.mark.parametrize(
+        "testcasename,storageType,name,locationType,mountPath,path,UNC_path,storageProtocol,saveFlag,excepetd_statuscode",
+        data_pass.add_storgae,ids=[tc[0] for tc in data_pass.add_storgae]  # Using test case name for readability in reports 
+        )       
+    def test_add_storage(self,testcasename,storageType,name,locationType,mountPath,path,UNC_path,storageProtocol,saveFlag,excepetd_statuscode):
+        params=Payload.add_storage_params(storageType)
+        body=Payload.add_storage_body(name,locationType,mountPath,path,UNC_path,storageProtocol,saveFlag)
+        add_storage_response=call_method.test_post_method("/v1/manage-storage",params=params,json=body)
+        log_request_response(add_storage_response,request_body=body,params=params)
+        assert add_storage_response.status_code==excepetd_statuscode    
+
+    @pytest.mark.parametrize("testcasename,storageId,excepetd_statuscode",
+        data_pass.fetch_storage,ids=[tc[0] for tc in data_pass.fetch_storage]
+        )
+    def test_fetch_storage(self,testcasename,storageId,excepetd_statuscode):
+        params=Payload.fetch_storage_params(storageId)
+        fetch_storage_response=call_method.test_get_method("/v1/manage-storage",params=params)
+        log_request_response(fetch_storage_response,params=params)
+        assert fetch_storage_response.status_code==excepetd_statuscode
+
+    @pytest.mark.parametrize("testcasename,platformType,optimServerName, optimDirectory, psthomePath, defaultQualifier, storageId, excepetd_statuscode",
+        data_pass.convert_server,ids=[tc[0] for tc in data_pass.convert_server]
+        )
+    def test_convert_server(self,testcasename,platformType,optimServerName, optimDirectory, psthomePath, defaultQualifier, storageId, excepetd_statuscode):
+        params=Payload.convert_server_params(platformType)
+        body=Payload.convert_server_body(optimServerName, optimDirectory, psthomePath, defaultQualifier, storageId)
+        convert_server_response=call_method.test_post_method("/v1/convert-server",params=params,json=body)
+        log_request_response(convert_server_response,request_body=body,params=params)
+        assert convert_server_response.status_code==excepetd_statuscode
+      
+    @pytest.mark.parametrize("testcasename,catalogType,skip,ssl,queryServerName,databaseName,host,port,username,psswrd,excepetd_statuscode",
+        data_pass.query_server,ids=[tc[0] for tc in data_pass.query_server]
+        )
+    def test_query_server(self,testcasename,catalogType,skip,ssl,queryServerName,databaseName,host,port,username,psswrd,excepetd_statuscode):
+        params=Payload.query_server_params(catalogType,skip,ssl)
+        body=Payload.query_server_body(queryServerName,databaseName,host,port,username,psswrd)
+        query_server_response=call_method.test_post_method("/v1/query-server",params=params,json=body)
+        log_request_response(query_server_response,request_body=body,params=params)
+        assert query_server_response.status_code==excepetd_statuscode
--- utils/request.py
+++ utils/request.py
@@ -5,13 +5,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 class request_call():
-    Base_url="https://archive174.estuate.com:3051"
+    Base_url="https://archive208.estuate.com:3051"
 
-    def test_get_method(self,endpoint,headers=None):
+    def test_get_method(self,endpoint,headers=None,params=None):
         from test_api import Testarchive
         headers = headers or {"Authorization": Testarchive.auth_token}
         url=f"{self.Base_url}/{endpoint}"
-        get_response=requests.get(url=url,verify=False,headers=headers)
+        get_response=requests.get(url=url,verify=False,headers=headers,params=params)
         return get_response
     
     def test_post_method(self,endpoint, json=None, headers=None,params=None):
